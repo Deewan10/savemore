a0aa90c9f793d7e9a018ba278577925cfbe0d0f8
peterstemidayo28@gmail.com <peterstemidayo28@gmail.com>
2024-08-15 00:00:30 +0100

    bug

---
+++ src/Components/Budget.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Budget = () => {
+  return (
+    <div className='budget'>
+
+        <h1>Budget</h1>
+        <div className='landing-page'>
+            <div className='img'>
+
+            </div>
+            <div className='inputs'>
+                <div className='income'>
+                
+                </div>
+                <div className='expenses'>
+                
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Budget
